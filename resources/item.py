9c8ccfde315772e09503903abb9dd936805f0de9
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2023-02-07 15:51:30 -0300

    Resposta atualizadas, para retonar com o marshmallow

--- resources/item.py
+++ resources/item.py
@@ -9,9 +9,10 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @blp.response(200, ItemSchemas)
     def get(self, item_id):
         try:
-            return {"item": items[item_id]}, 201
+            return {"item": items[item_id]}
         except KeyError:
             abort(404, message="Store not found")
 
@@ -21,24 +22,27 @@ class Item(MethodView):
             return {"message": "Item deleted."}
         except KeyError:
             abort(404, message="Item not found.")
-
+# https://blog.teclado.com/python-dictionary-merge-update-operators/
     @blp.arguments(ItemUpdateSchemas)
+    @blp.response(200, ItemSchemas)
     def put(self, item_data, item_id):
         try:
             item = items[item_id]
             item |= item_data
 
-            return {"item": item}, 201
+            return {"item": item}
         except KeyError:
             abort(404, message="Item not found.")
 
 
 @blp.route("/item")
 class  ItemList(MethodView):
+    @blp.response(200, ItemSchemas(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return items.values()
 
     @blp.arguments(ItemSchemas)
+    @blp.response(201, ItemSchemas)
     def post(self, item_data):
         for item in items.values():
             if (item_data["name"] == items["name"] and item_data["store_id"] == items["store_id"]):
@@ -46,4 +50,4 @@ class  ItemList(MethodView):
         item_id = uuid.uuid4().hex
         item = {**item_data, "id": item_id}
         items[item_id] = item
-        return item, 201
+        return item
--- resources/store.py
+++ resources/store.py
@@ -9,12 +9,13 @@ blp = Blueprint("Stores", __name__, description="Operations on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchemas)
     def get(self, store_id):
         try:
-            return {"store": stores[store_id]}, 201
+            return stores[store_id]
         except KeyError:
             abort(404, message="Store not found.")
-    
+
     def delete(self, store_id):
         try:
             del stores[store_id]
@@ -23,21 +24,25 @@ class Store(MethodView):
             abort(404, message="Store not found.")
 
     @blp.arguments(StoreSchemas)
+    @blp.response(200, StoreSchemas)
     def put(self, store_data, store_id):
         try:
             store = stores[store_id]
             store |= store_data
 
-            return {"store": store}, 201
+            return {"store": store}
         except KeyError:
             abort(404, message="Store not found.")
 
 @blp.route("/store")
 class StoresList(MethodView):
+    @blp.response(200, StoreSchemas(many=True))
     def get(self):
-        return {"stores": list(stores.values())}
+        return stores.values()
+
 
     @blp.arguments(StoreSchemas)
+    @blp.response(201, StoreSchemas)
     def post(self, store_data): 
         for store in stores.values():
             if store["name"] == store_data["name"]:
@@ -45,4 +50,4 @@ class StoresList(MethodView):
         store_id = uuid.uuid4().hex # Valor temporário como id
         store = {**store_data, "id": store_id}
         stores[store_id] = store
-        return store, 201
+        return store
