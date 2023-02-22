8a8fda07254bb06c6aa8c534045a16ed6475730e
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2023-02-22 17:34:10 -0300

    Etiquetas adicionandas, nada da implementação é diferente.

--- schemas.py
+++ schemas.py
@@ -9,13 +9,22 @@ class PlainItemSchema(Schema):
 
 class PlainStoreSchema(Schema):
     id = fields.Int(dump_only=True)
-    name = fields.Str()
+    name = fields.Str(required=True)
+
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
 
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
     store_id = fields.Int()
+
+
+class StoreUpdateSchema(Schema):
+    name = fields.Str()
 
 
 class ItemSchema(PlainItemSchema):
@@ -25,3 +34,9 @@ class ItemSchema(PlainItemSchema):
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
